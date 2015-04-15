12aba5ccf82add6d9275e85489291927144cd8c9
Adriel G. Cruz <ejecruz@yahoo.com>
2015-04-15 13:22:41 +0800

    updated CreateDummyData.java

--- ocnt-ws/src/main/java/com/dhl/webservice/dummy/CreateDummyData.java
+++ ocnt-ws/src/main/java/com/dhl/webservice/dummy/CreateDummyData.java
@@ -2,128 +2,152 @@ package com.dhl.webservice.dummy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.dhl.ocnt.model.Filter;
 import com.dhl.ocnt.model.Movement;
 
 public class CreateDummyData {
 	
+
+	
 	//create dummy data
 	public static List<Movement> createDummyData(){
 		List<Movement> mvList = new ArrayList<Movement>();
 		
+		int no_records = randInt(100, 1000);
+		int no_pages = no_records/10;
+		
 		//create ramdom data
-		for(int i = 0; i<100; i++){
+		for(int i = 0; i<no_records; i++){
 			
 			Movement mv = new Movement();
 			
-			if((i%9) == 0){
+			if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN004/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("C");
 				mv.setSTD("23:00");
 				mv.setDestination("SINHUB");
-				mv.setMawb("111-"+(987658321+i)+"");
+				mv.setMawb("119-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OP");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%8) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN005/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("24:00");
 				mv.setDestination("MNLHUB");
-				mv.setMawb("112-"+(987654371+i)+"");
+				mv.setMawb("118-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OX");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%7) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN006/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("22:00");
 				mv.setDestination("TPEHUB");
-				mv.setMawb("113-"+(987654328+i)+"");
+				mv.setMawb("117-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OJ");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%6) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN007/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("21:00");
 				mv.setDestination("KLUHUB");
-				mv.setMawb("114-"+(987654327+i)+"");
+				mv.setMawb("116-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OU");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%5) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN008/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("20:00");
 				mv.setDestination("CYBHUB");
-				mv.setMawb("115-"+(987654327+i)+"");
+				mv.setMawb("115-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OY");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%4) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN009/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("19:00");
 				mv.setDestination("USAHUB");
-				mv.setMawb("116-"+(987654326+i)+"");
+				mv.setMawb("114-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OT");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%3) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN010/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("18:00");
 				mv.setDestination("HUBHUB");
-				mv.setMawb("117-"+(987654325+i)+"");
+				mv.setMawb("113-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OR");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
-			else if((i%2) == 0){
+			else if((i%(randInt(1, 9))) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN011/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("17:00");
 				mv.setDestination("LETHUB");
-				mv.setMawb("118-"+(987654324+i)+"");
+				mv.setMawb("112-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OD");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else{
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN012/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("X");
 				mv.setSTD("16:00");
 				mv.setDestination("HKGHUB");
-				mv.setMawb("119-"+(987654323+i)+"");
+				mv.setMawb("111-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OV");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			
 			mvList.add(mv);
@@ -134,120 +158,142 @@ public class CreateDummyData {
 	
 	//create dummy data with parameters
 	public static List<Movement> createDummyData(String plus_date, String minus_date){
+		
 		List<Movement> mvList = new ArrayList<Movement>();
 		
+		int no_records = randInt(100, 1000);
+		int no_pages = no_records/10;
+		
 		//create ramdom data
-		for(int i = 0; i<100; i++){
+		for(int i = 0; i<no_records; i++){
 			
 			Movement mv = new Movement();
 			
 			if((i%9) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN004/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("C");
 				mv.setSTD("23:00");
 				mv.setDestination("SINHUB");
-				mv.setMawb("111-"+(987658321+i)+"");
+				mv.setMawb("119-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OP");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%8) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN005/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("24:00");
 				mv.setDestination("MNLHUB");
-				mv.setMawb("112-"+(987654371+i)+"");
+				mv.setMawb("118-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OX");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%7) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN006/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("22:00");
 				mv.setDestination("TPEHUB");
-				mv.setMawb("113-"+(987654328+i)+"");
+				mv.setMawb("117-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OJ");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%6) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN007/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("21:00");
 				mv.setDestination("KLUHUB");
-				mv.setMawb("114-"+(987654327+i)+"");
+				mv.setMawb("116-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OU");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%5) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN008/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("20:00");
 				mv.setDestination("CYBHUB");
-				mv.setMawb("115-"+(987654327+i)+"");
+				mv.setMawb("115-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OY");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%4) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN009/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("19:00");
 				mv.setDestination("USAHUB");
-				mv.setMawb("116-"+(987654326+i)+"");
+				mv.setMawb("114-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OT");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%3) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN010/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("18:00");
 				mv.setDestination("HUBHUB");
-				mv.setMawb("117-"+(987654325+i)+"");
+				mv.setMawb("113-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OR");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else if((i%2) == 0){
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN011/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("A");
 				mv.setSTD("17:00");
 				mv.setDestination("LETHUB");
-				mv.setMawb("118-"+(987654324+i)+"");
+				mv.setMawb("112-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OD");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			else{
 				mv.setOperation("");
-				mv.setMovementNo(""+i);
+				mv.setMovementNo("SN012/"+(randInt(5,70)+i));
 				mv.setTransptReg("");
 				mv.setTransptType("X");
 				mv.setSTD("16:00");
 				mv.setDestination("HKGHUB");
-				mv.setMawb("119-"+(987654323+i)+"");
+				mv.setMawb("111-"+(987658321+randInt(1,10)+i)+"");
 				mv.setMfst("");
 				mv.setCutOffTime("22:50/13");
 				mv.setStatus("OV");
+				mv.setTotalNoOfPage(no_pages);
+				mv.setTotalNoOfRec(no_records);
 			}
 			
 			mv.setPlus_date(plus_date);
@@ -273,6 +319,19 @@ public class CreateDummyData {
 		
 		return fil;
 		
+	}
+	
+	public static int randInt(int min, int max) {
+
+	    // NOTE: Usually this should be a field rather than a method
+	    // variable so that it is not re-seeded every call.
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
 	}
 
 }
