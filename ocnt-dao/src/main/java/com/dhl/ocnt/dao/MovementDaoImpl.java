831e50c64ed0e9e784ca697a0c5e7057e05ed196
jesus-dayo <jesus_dayo@yahoo.com>
2015-04-27 14:51:27 +0800

    modified datasource

--- ocnt-dao/src/main/java/com/dhl/ocnt/dao/MovementDaoImpl.java
+++ ocnt-dao/src/main/java/com/dhl/ocnt/dao/MovementDaoImpl.java
@@ -18,15 +18,25 @@ public class MovementDaoImpl implements MovementDao{
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	@Autowired
-	private Properties movementQuery;
+	private String movementCountSQL = "SELECT COUNT(1) "+
+			"FROM MOVEMENT M INNER JOIN MOVEMENT_DOCUMENT MD "+
+			"ON M.MOVEMENT_ID = MD.MOVEMENT_ID WHERE " +
+			"m.MVMT_SCHD_DEPARTURE_DT BETWEEN TRUNC(SYSDATE) -? AND TRUNC(SYSDATE) +? "+
+			"AND m.ORI_FAC_ID=?";
+
+	private String movementSQL = "SELECT MOVEMENT_NUMBER,M.MOVEMENT_ID,FLEET_TYPE,VEHICLE_ID,M.MVMT_SCHD_DEPARTURE_DT, " +
+			"M.DEST_FAC_ID, MOVEMENT_DOCUMENT_ID, MVMT_STATUS_CODE, CLOSE_DT, DEPART_DT, CUT_OFF_TIME " +
+			"FROM MOVEMENT M INNER JOIN MOVEMENT_DOCUMENT MD " +
+			"ON M.MOVEMENT_ID = MD.MOVEMENT_ID WHERE " +
+			"m.MVMT_SCHD_DEPARTURE_DT BETWEEN TRUNC(SYSDATE) -? AND TRUNC(SYSDATE) +? " +
+			"AND m.ORI_FAC_ID=?";
 	
 	@Override
 	public Page<Movement> getMovementList(int minusDate, int plusDate,
 			final int pageNo,final int pageSize,String location) {
 		PaginationHelper<Movement> ph = new PaginationHelper<Movement>();
-		return ph.fetchPage(jdbcTemplate, movementQuery.getProperty("getMovementCount"), 
-								movementQuery.getProperty("getMovement"), 
+		return ph.fetchPage(jdbcTemplate, movementCountSQL, 
+				movementSQL, 
 								new Object[]{}, pageNo, pageSize, new RowMapper<Movement>(){
 
 									@Override
